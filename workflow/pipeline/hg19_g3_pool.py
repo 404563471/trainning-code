b870aaf59f930beddfb7190ba2bcdafa0697b785
404563471 <404563471@qq.com>
2020-07-23 21:00:57 +0800

    pipelines

--- workflow/pipeline/hg19_g3_pool.py
+++ workflow/pipeline/hg19_g3_pool.py
@@ -1,16 +1,7 @@
 import os
 from multiprocessing.dummy import Pool as ThreadPool
 
-samplePath="./v3-g3/"
-ref_path="./ref/hg19_centromere.fa"
-alignedPath="./results/hg19_align/"
-pbsvcallPath="./results/hg19_sv/"
 
-if not os.path.exists(alignedPath):
-    os.mkdir(alignedPath)
-    
-if not os.path.exists(pbsvcallPath):
-    os.mkdir(pbsvcallPath)
 
 def bamPath(rootPath):
     filelist=[]
@@ -23,28 +14,42 @@ def bamPath(rootPath):
                 filelist.append(" ".join([subreadsPath, ref_path, alignOutput]))
     return filelist
 
-
 def pbmmAlign(filelist):
     os.system(" ".join(['pbmm2 align ', filelist, ' --sort -j 10 -J 5']))
 
-
-pool=ThreadPool(processes=4)
-pool.map(pbmmAlign, bamPath(samplePath))
-pool.close()
-pool.join()
-
-
-alignedfiles=os.listdir(alignedPath)
 def svdiscover(file):
     filename=os.path.basename(file)
     svPath=os.path.join(pbsvcallPath, filename+".svsig.gz")
     filePath=os.path.join(alignedPath, file)
     os.system(" ".join(["pbsv discover", filePath, svPath]))
 
-pool=ThreadPool()
-pool.map(svdiscover, alignedfiles)
-pool.close()
-pool.join()
 
-svfiles=[os.path.join(pbsvcallPath, svfile) for svfile in os.listdir(pbsvcallPath)]
-os.system(" ".join(["pbsv call -j 10", ] + svfiles + [os.path.join(pbsvcallPath, "merge.vcf"),]))
+allSamplePath = "./data/human/rawdata/"
+ref_path="./data/human/ref/hg19_centromere.fa"
+resultPath="./results"
+
+for sample in os.listdir(allSamplePath):
+    samplePath=os.path.join(allSamplePath, sample)
+    alignedPath=os.path.join("./results/hg19_align/", sample)
+    pbsvcallPath=os.path.join("./results/hg19_sv/", sample)
+
+    if not os.path.exists(alignedPath):
+        os.makedirs(alignedPath)
+        
+    if not os.path.exists(pbsvcallPath):
+        os.makedirs(pbsvcallPath)
+
+    pool=ThreadPool(processes=6)
+    pool.map(pbmmAlign, bamPath(samplePath))
+    pool.close()
+    pool.join()
+
+    alignedfiles=os.listdir(alignedPath)
+
+    pool=ThreadPool()
+    pool.map(svdiscover, alignedfiles)
+    pool.close()
+    pool.join()
+
+    svfiles=[os.path.join(pbsvcallPath, svfile) for svfile in os.listdir(pbsvcallPath)]
+    os.system(" ".join(["pbsv call -j 20", ref_path] + svfiles + [os.path.join(pbsvcallPath, "merge.vcf"),]))
